7164fa605593d9294847936004d4266835e7e9db
Robert Borghese <28355157+RobertBorghese@users.noreply.github.com>
2023-05-05 06:30:57 -0400

    Split up CSClass content into multiple functions

--- src/cscompiler/components/CSClass.hx
+++ src/cscompiler/components/CSClass.hx
@@ -19,18 +19,45 @@ using reflaxe.helpers.SyntaxHelper;
 **/
 class CSClass extends CSBase {
 	/**
+		The list of variables compiled into C# accumulated while compiling the class.
+	**/
+	var variables: Array<String> = [];
+
+	/**
+		The list of functions compiled into C# accumulated while compiling the class.
+	**/
+	var functions: Array<String> = [];
+
+	/**
+		C# name of the class currently being compiled.
+	**/
+	var csClassName: String = "";
+
+	/**
+		C# namespace of the class  currently being compiled.
+	**/
+	var csNameSpace: String = "";
+
+	/**
+		Called at the start of a class' compilation to reset the variables.
+	**/
+	function init(classType: ClassType) {
+		variables = [];
+		functions = [];
+
+		final className = classType.name;
+		csClassName = compiler.compileClassName(classType);
+
+		final classPack = classType.pack;
+		csNameSpace = compiler.packToNameSpace(classPack);
+	}
+
+	/**
 		Implementation of `CSCompiler.compileClassImpl`.
 	**/
 	public function compile(classType: ClassType, varFields: Array<ClassVarData>, funcFields: Array<ClassFuncData>): Null<String> {
 		// Stores all the variables and fields to put together later.
-		final variables = [];
-		final functions = [];
-
-		final className = classType.name;
-		final csClassName = compiler.compileClassName(classType);
-
-		final classPack = classType.pack;
-		final csNameSpace = compiler.packToNameSpace(classPack);
+		init(classType);
 
 		var declaration = "";
 
@@ -46,84 +73,12 @@ class CSClass extends CSBase {
 
 		// Variables
 		for(v in varFields) {
-			final field = v.field;
-
-			// Compile name
-			final varName = compiler.compileVarName(field.name, null, field);
-
-			// Compile type
-			final varType = compiler.compileType(field.type, field.pos);
-
-			// Compile expression
-			final e = field.expr();
-			final csExpr = if(e != null) {
-				compiler.compileClassVarExpr(e);
-			} else {
-				"";
-			}
-
-			// Compile metadata
-			final meta = compiler.compileMetadata(field.meta, MetadataTarget.ClassField) ?? "";
-
-			// Put it all together to make C# variable
-			final decl = meta + (v.isStatic ? "static " : "") + (varType ?? "var") + " " + varName + (csExpr.length == 0 ? "" : (" = " + csExpr)) + ";";
-			variables.push(decl);
+			compileVariable(v);
 		}
 
 		// Functions
 		for(f in funcFields) {
-			final field = f.field;
-
-			// Compile name
-			final name = field.name == "new" ? csClassName : compiler.compileVarName(field.name);
-
-			// Compile metadata
-			final meta = compiler.compileMetadata(field.meta, MetadataTarget.ClassField) ?? "";
-
-			// If a dynamic function, we want to compile as function variable.
-			if(f.kind == MethDynamic) {
-				// `field.expr()` gives an expression of as TypedExprDef.TFunction,
-				// so we don't need to do anything special to compile it as a lambda.
-				final e = field.expr();
-				if(e != null) {
-					final callable = compiler.compileClassVarExpr(e);
-					final decl = meta + (f.isStatic ? "static " : "") + "var " + name + " = " + callable;
-					variables.push(decl);
-				}
-			} else {
-				// Compile arguments
-				final arguments = f.args.map(a -> compiler.compileFunctionArgument(a.type, a.name, field.pos, a.opt, a.expr));
-
-				// Compile return type
-				final ret = compiler.compileType(f.ret, field.pos);
-
-				// Compile expression - Use `data.expr` instead of `field.expr()`
-				// since it gives us the contents of the function.
-				final csExpr = {
-					if(f.expr != null) {
-						final code = compiler.compileClassFuncExpr(f.expr);
-						"{\n" + code.tab() + "\n}";
-					} else {
-						";";
-					}
-				}
-
-				// Function properties
-				final props = [ "public" ]; // Always public
-
-				if(f.isStatic) {
-					props.push("static");
-				} else {
-					// Add virtual if @:virtual meta OR has child override
-					if(field.hasMeta(":virtual") || ClassHierarchyTracker.funcHasChildOverride(classType, field, false)) {
-						props.push("virtual");
-					}
-				}
-
-				// Put it all together to make the C# function
-				final func = meta + props.join(" ") + " " + ret + " " + name + "(" + arguments.join(", ") + ") " + csExpr;
-				functions.push(func);
-			}
+			compileFunction(f, classType);
 		}
 
 		// if there are no instance variables or functions,
@@ -153,6 +108,101 @@ class CSClass extends CSBase {
 
 			result;
 		}
+	}
+
+	/**
+		Compiles the class variable.
+	**/
+	function compileVariable(v: ClassVarData) {
+		final field = v.field;
+
+		// Compile name
+		final varName = compiler.compileVarName(field.name, null, field);
+
+		// Compile type
+		final varType = compiler.compileType(field.type, field.pos);
+
+		// Compile expression
+		final e = field.expr();
+		final csExpr = if(e != null) {
+			compiler.compileClassVarExpr(e);
+		} else {
+			"";
+		}
+
+		// Compile metadata
+		final meta = compiler.compileMetadata(field.meta, MetadataTarget.ClassField) ?? "";
+
+		// Put it all together to make C# variable
+		final decl = meta + (v.isStatic ? "static " : "") + (varType ?? "var") + " " + varName + (csExpr.length == 0 ? "" : (" = " + csExpr)) + ";";
+		variables.push(decl);
+	}
+
+	/**
+		Compiles the class function.
+	**/
+	function compileFunction(f: ClassFuncData, classType: ClassType) {
+		final field = f.field;
+
+		// Compile name
+		final name = field.name == "new" ? csClassName : compiler.compileVarName(field.name);
+
+		// Compile metadata
+		final meta = compiler.compileMetadata(field.meta, MetadataTarget.ClassField) ?? "";
+
+		// If a dynamic function, we want to compile as function variable.
+		if(f.kind == MethDynamic) {
+			// `field.expr()` gives an expression of as TypedExprDef.TFunction,
+			// so we don't need to do anything special to compile it as a lambda.
+			final e = field.expr();
+			if(e != null) {
+				final callable = compiler.compileClassVarExpr(e);
+				final decl = meta + (f.isStatic ? "static " : "") + "var " + name + " = " + callable;
+				variables.push(decl);
+			}
+		} else {
+			// Compile arguments
+			final arguments = f.args.map(a -> compiler.compileFunctionArgument(a.type, a.name, field.pos, a.opt, a.expr));
+
+			// Compile return type
+			final ret = compiler.compileType(f.ret, field.pos);
+
+			// Compile expression - Use `data.expr` instead of `field.expr()`
+			// since it provides the contents of the function.
+			final csExpr = {
+				if(f.expr != null) {
+					final code = compiler.compileClassFuncExpr(f.expr);
+					"{\n" + code.tab() + "\n}";
+				} else {
+					";";
+				}
+			}
+
+			// Put it all together to make the C# function
+			final props = compileFunctionProperties(f, classType).join(" ");
+			final func = meta + props + " " + ret + " " + name + "(" + arguments.join(", ") + ") " + csExpr;
+			functions.push(func);
+		}
+	}
+
+	/**
+		Returns a list of all the C# properties to be appened to a C# function.
+	**/
+	function compileFunctionProperties(f: ClassFuncData, classType: ClassType): Array<String> {
+		final field = f.field;
+
+		final props = [ "public" ]; // Always public
+
+		if(f.isStatic) {
+			props.push("static");
+		} else {
+			// Add virtual if @:virtual meta OR has child override
+			if(field.hasMeta(":virtual") || ClassHierarchyTracker.funcHasChildOverride(classType, field, false)) {
+				props.push("virtual");
+			}
+		}
+
+		return props;
 	}
 }
 
