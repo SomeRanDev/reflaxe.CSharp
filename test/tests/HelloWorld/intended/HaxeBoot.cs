0c1dffe594971714c5be81fa3033aa7d05535a44
Robert Borghese <28355157+RobertBorghese@users.noreply.github.com>
2023-05-05 23:03:21 -0400

    Update intended content for tests

---
+++ test/tests/HelloWorld/intended/HaxeBoot.cs
@@ -0,0 +1,7 @@
+namespace haxe.root {
+	class HaxeBoot {
+		static void Main(string[] args) {
+			Main_Fields_.main();
+		}
+	}
+}
---
+++ test/tests/Misc/intended/HaxeBoot.cs
@@ -0,0 +1,7 @@
+namespace haxe.root {
+	class HaxeBoot {
+		static void Main(string[] args) {
+			Main.main();
+		}
+	}
+}
