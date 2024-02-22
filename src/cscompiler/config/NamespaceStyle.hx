7c5b739d5f5302fb152f51a6891e7a4f5b2e1952
Robert Borghese <28355157+SomeRanDev@users.noreply.github.com>
2024-02-21 22:54:12 -0500

    Use tabs consistently

--- src/cscompiler/config/NamespaceStyle.hx
+++ src/cscompiler/config/NamespaceStyle.hx
@@ -1,19 +1,19 @@
 package cscompiler.config;
 
 /**
-    Represents the value for the `-D namespace_style` define.
+	Represents the value for the `-D namespace_style` define.
 **/
 enum NamespaceStyle {
-    Default;
-    Pascal;
+	Default;
+	Pascal;
 }
 
 /**
-    Converts a `String` from a define to the `NamespaceStyle` enum.
+	Converts a `String` from a define to the `NamespaceStyle` enum.
 **/
 function fromString(defineValue: String) {
-    return switch(defineValue.toLowerCase()) {
-        case "pascal": Pascal;
-        case _: Default;
-    }
+	return switch(defineValue.toLowerCase()) {
+		case "pascal": Pascal;
+		case _: Default;
+	}
 }
