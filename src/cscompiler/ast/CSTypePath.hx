02437908633bafdc0e80d6f3f3bb3affcc58b570
Jeremy Faivre <contact@jeremyfa.com>
2024-03-16 19:04:53 +0100

    Add a note about `CSTypePath`, that acts as a reference to a type.

--- src/cscompiler/ast/CSTypePath.hx
+++ src/cscompiler/ast/CSTypePath.hx
@@ -1,6 +1,10 @@
 package cscompiler.ast;
 
 /**
-    A reference to a C# type described by the given type path
+    A reference to a C# type described by the given type path.
+
+    This is mostly used as reference to an actual type without needing
+    the underlying type right away. Once the whole C# AST has been generated,
+    an actual type can be retrieved from its type path (at printing stage).
 **/
 typedef CSTypePath = String;
